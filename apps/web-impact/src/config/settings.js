8245dc946a91617c0ad824d496adcea7c9473d4d
Jacob Ford <j@cobford.com>
2024-02-08 12:45:08 -0500

    Short name

--- apps/web-impact/src/config/settings.js
+++ apps/web-impact/src/config/settings.js
@@ -4,7 +4,7 @@ export default {
 	title: `SRF Impact`,
 	description: `Built on justice and dedicated to building the equitable economies of the future.`,
 	url: astroConfig.site?.replace(/\/+$/,''), // No trailing slash!
-	name: `SRF Impact`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	name: `SRF`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
