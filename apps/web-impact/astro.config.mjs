f2cae7951cdd44e0432c317cb22c8804f924da64
Jacob Ford <j@cobford.com>
2024-02-13 19:29:25 -0500

    Remove @astrojs/image

--- apps/web-impact/astro.config.mjs
+++ apps/web-impact/astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
-import image from "@astrojs/image";
 import lit from "@astrojs/lit";
 
 const VERCEL_PREVIEW_SITE =
@@ -16,7 +15,11 @@ export default defineConfig({
   site: siteURL,
   sitemap: true,
   // Generate sitemap (set to "false" to disable)
-  integrations: [sitemap(), mdx(), image(), lit()], // Add renderers to the config
+  integrations: [
+    sitemap(),
+    mdx(),
+    lit()
+  ], // Add renderers to the config
   // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
   vite: {
     ssr: {
