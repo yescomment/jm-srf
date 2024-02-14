7361fe9c42d9bdc42b0f6cb6092ed0f6733bfb4b
Jacob Ford <j@cobford.com>
2024-02-13 19:31:19 -0500

    Disable theme switcher

--- apps/web-impact/src/config/settings.js
+++ apps/web-impact/src/config/settings.js
@@ -5,6 +5,6 @@ export default {
 	description: `Built on justice and dedicated to building the equitable economies of the future.`,
 	url: astroConfig.site?.replace(/\/+$/,''), // No trailing slash!
 	name: `SRF`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-  enableThemeSwitcher: true,
+  enableThemeSwitcher: false,
   showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
