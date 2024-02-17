4aa233252b02b7a7d5ba5b7b6100ee3611133ee5
Jacob Ford <j@cobford.com>
2024-02-16 23:02:46 -0500

    Change capital name

--- apps/web-capital/src/config/settings.js
+++ apps/web-capital/src/config/settings.js
@@ -1,7 +1,7 @@
 import astroConfig from "../../astro.config.mjs";
 
 export default {
-	title: `SRF Impact`,
+	title: `SRF Capital`,
 	description: `Built on justice and dedicated to building the equitable economies of the future.`,
 	url: astroConfig.site?.replace(/\/+$/,''), // No trailing slash!
 	name: `SRF`, // The short name of the business or brand name. Used for things like the copyright in the footer.
