ed5b8d4073e46c0dd55880f7524d8982ff346d4d
Jacob Ford <j@cobford.com>
2024-02-16 21:20:25 -0500

    Duplicate impact into capital, create package for shared components

---
+++ apps/web-capital/astro.config.mjs
@@ -0,0 +1,29 @@
+import { defineConfig } from 'astro/config';
+import sitemap from "@astrojs/sitemap";
+import mdx from "@astrojs/mdx";
+import lit from "@astrojs/lit";
+
+const VERCEL_PREVIEW_SITE =
+  process.env.VERCEL_ENV !== 'production' &&
+  process.env.VERCEL_URL &&
+  `https://${process.env.VERCEL_URL}`;
+
+const siteURL = VERCEL_PREVIEW_SITE || 'https://srfimpact.org/';
+
+// https://astro.build/config
+export default defineConfig({
+  site: siteURL,
+  sitemap: true,
+  // Generate sitemap (set to "false" to disable)
+  integrations: [
+    sitemap(),
+    mdx(),
+    lit()
+  ], // Add renderers to the config
+  // This is for the astro-icon package. You can find more about the package here: https://www.npmjs.com/package/astro-icon
+  vite: {
+    ssr: {
+      external: ["svgo"],
+    },
+  },
+});
---
+++ apps/web-capital/src/config/nav.js
@@ -0,0 +1,26 @@
+export const nav = [
+	{
+		title: 'Home',
+		slug: '/',
+	},
+	{
+		title: 'About',
+		slug: '/about',
+	},
+	{
+		title: 'Strategy',
+		slug: '/strategy',
+	},
+	{
+		title: 'Impact',
+		slug: '/impact',
+	},
+	{
+		title: 'The (W)Health Papers',
+		slug: '/whealth-papers',
+	},
+	{
+		title: 'Contact',
+		slug: '/company/contact',
+	},
+];
