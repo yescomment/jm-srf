bdcf77064e48da895789002408708f16a0cf176b
Jacob Ford <j@cobford.com>
2024-02-21 16:35:54 -0500

    Make Header consistant, add config for site URLs

--- packages/jm-srf-components/index.js
+++ packages/jm-srf-components/index.js
@@ -1,4 +1,5 @@
 export { default as colors } from './colors'
+export { default as config } from './config'
 
 export { default as Header } from './Header.astro'
 export { default as PageSection } from './PageSection.astro'
