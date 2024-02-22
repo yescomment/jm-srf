b4ca95d520541ac693b3e56c699ce48b3e8cf83f
Jacob Ford <j@cobford.com>
2024-02-22 15:10:47 -0500

    Globalize BaseHtml (Base/Layout) and BaseHead, and all styles except themes

--- packages/jm-srf-components/index.js
+++ packages/jm-srf-components/index.js
@@ -1,6 +1,9 @@
 export { default as colors } from './colors'
 export { default as config } from './config'
 
+export { default as BaseHtml } from './BaseHtml.astro'
+export { default as BaseHead } from './BaseHead.astro'
+
 export { default as Header } from './Header.astro'
 export { default as PageSection } from './PageSection.astro'
 export { default as HeroSection } from './HeroSection.astro'
