75c0860cb6afe18b67c19bd45201b6a91c65e08a
Jacob Ford <j@cobford.com>
2024-02-22 10:38:50 -0500

    Create HeroSection with background image

--- packages/jm-srf-components/index.js
+++ packages/jm-srf-components/index.js
@@ -3,3 +3,4 @@ export { default as config } from './config'
 
 export { default as Header } from './Header.astro'
 export { default as PageSection } from './PageSection.astro'
+export { default as HeroSection } from './HeroSection.astro'
