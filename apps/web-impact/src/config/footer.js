3b867da78beb9fdb35d4fe5e5e7fe2246adfb7a0
Jacob Ford <j@cobford.com>
2024-02-23 17:26:59 -0500

    Update footers

--- apps/web-impact/src/config/footer.js
+++ apps/web-impact/src/config/footer.js
@@ -1,80 +1,56 @@
 export const footerSocials = [
   {
-    name: 'Twitter',
-    url: 'https://twitter.com/littlesticksdev',
-    icon: "mdi:twitter",
-  },
-  {
-    name: 'Youtube',
-    url: 'https://youtube.com/c/LittleSticks',
-    icon: "mdi:youtube",
-  },
-  {
-    name: 'Github',
-    url: 'https://github.com/littlesticks',
-    icon: "mdi:github",
-  },
-  {
-    name: 'Discord',
-    url: 'https://littlesticks.dev/discord',
-    icon: "ic:baseline-discord",
-  },
+    name: 'Email',
+    url: 'mailto:dorian@whealthfund.com',
+    icon: "mdi:email",
+  }
 ]
 
 export const footerLists = [
 	{
-		title: 'Landing Pages',
-		items: [
-			{
-				title: 'Landing Page 1',
-				slug: '/landing-pages/landing-1',
-			},
-			{
-				title: 'Landing Page 2',
-				slug: '/landing-pages/landing-2',
-			},
-      // {
-			// 	title: 'Landing Page 3',
-			// 	slug: '/landing-pages/landing-3',
-			// },
-		],
-	},
-	{
-		title: 'Company',
+		title: 'SRF Impact',
 		items: [
 			{
 				title: 'About',
-				slug: '/company/about',
+				slug: '/about',
 			},
 			{
-				title: 'Blog',
-				slug: '/blog',
+				title: 'Strategy',
+				slug: '/strategy',
 			},
-			{
-				title: 'Contact',
-				slug: '/company/contact',
-			},
+      {
+        title: 'Impact',
+        slug: '/impact',
+      },
+      {
+        title: 'The (W)Health Papers',
+        slug: '/whealth-papers',
+      }
 		],
 	},
 	{
-		title: 'Theme',
+		title: 'SRF Capital',
 		items: [
 			{
-				title: 'Get Started',
-				slug: '/theme/get-started',
+				title: 'Meet',
+				slug: '/meet',
 			},
 			{
-				title: 'Style Guide',
-				slug: '/theme/style-guide',
+				title: 'Strategize',
+				slug: '/strategize',
 			},
-      {
-        title: 'Theme Setup',
-        slug: '/theme/theme-setup',
-      },
-      {
-        title: 'Customizing Odyssey',
-        slug: '/theme/customizing-odyssey',
-      }
+			{
+				title: 'Theorize',
+				slug: '/theorize',
+			},
+			{
+				title: 'Measure',
+				slug: '/measure',
+			},
+			{
+				title: 'Community',
+				slug: '/community',
+			},
 		],
 	},
 ];
