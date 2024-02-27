30b367f443f645387b2129066fe0b4a55438549e
Jacob Ford <j@cobford.com>
2024-02-27 09:42:54 -0500

    Dev port

--- packages/jm-srf-components/config.ts
+++ packages/jm-srf-components/config.ts
@@ -4,8 +4,8 @@ const config = {
 }
 
 if (process.env?.NODE_ENV === 'development') {
-	config.srfCapitalUrl = `http://localhost:${process.env.PORT || 4321}`
-	config.srfImpactUrl = `http://localhost:${process.env.PORT || 4322}`
+	config.srfCapitalUrl = `http://localhost:${process.env.PORT || 4322}`
+	config.srfImpactUrl = `http://localhost:${process.env.PORT || 4321}`
 } else if (process.env?.VERCEL_ENV === 'preview') {
 	config.srfCapitalUrl = 'http://srfcapital.vercel.app'
 	config.srfImpactUrl = 'http://srfimpact.vercel.app'
