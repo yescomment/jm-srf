ef60c671047d016a43817b35c5d7101d997f4c7e
Jacob Ford <j@cobford.com>
2025-05-22 16:08:24 -0400

    Redirect /meet to /about
    Think I mixed up redirect lines in previous commit

--- apps/web-capital/astro.config.mjs
+++ apps/web-capital/astro.config.mjs
@@ -18,6 +18,7 @@ export default defineConfig({
     '/measure': '/whealth+',
     '/strategize': '/strategy',
     '/theorize': '/approach',
+    '/meet': '/about',
   },
   sitemap: false,
   // Generate sitemap (set to "false" to disable)
